a4f92ca430e40a3b09d349bb66e04c8a0cd62906
tkl <tluk92@gmail.com>
2022-08-27 17:12:44 -0400

    wip: search bar submits data

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,25 @@
+import {useState} from 'react';
 
 const SearchBar = (props) => {
+    const [ searchState, setSearchState ] = useState({
+        searchFor: ''
+    });
+
+    const handleChange = (e) => {
+        setSearchState({searchFor: e.target.value});
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // Get data declaration
+            console.log(searchState.searchFor);
+        //
+        setSearchState({searchFor: ''});
+    };
     return(
         <>
-        <form>
-            <input type="text"/>
+        <form onSubmit={handleSubmit}>
+            <input type="text" value={searchState.searchFor} onChange={handleChange}/>
             <input type="submit" value="Search" />
         </form>
         </>
